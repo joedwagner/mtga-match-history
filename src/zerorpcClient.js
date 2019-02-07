9e38a0b6c62e7e2b40457b6d160540984b674e66
Joe Wagner <joedwagner12@gmail.com>
2019-02-07 13:52:32 -0600

    Delete old commented code

--- src/zerorpcClient.js
+++ src/zerorpcClient.js
@@ -1,19 +1,6 @@
 const zerorpc = require("zerorpc")
 const app = require('electron').remote.app
 
-// // const client = new zerorpc.Client()
-// // client.connect("tcp://127.0.0.1:4242")
-
-// client.invoke("init_db", app.getPath('userData'))
-// // client.invoke("insert_many", 100, (error, res) => {
-// //   if (error) {
-// //     console.log(error)
-// //   }
-// // })
-// client.invoke("get_all_matches", (error, res) => {
-//   console.log(res);
-// })
-
 /*****
 
 Section 1: Class Definition
