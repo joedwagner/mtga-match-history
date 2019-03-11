fdc10095b35edaba857e775d5343ffa81a7e680b
Joe Wagner <joedwagner12@gmail.com>
2019-03-11 17:16:03 -0500

    Create 'View' Components for different pages

---
+++ src/components/GraphsView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <p>We are in the Graphs view!</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'graphs-view'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MatchHistoryView.vue
@@ -0,0 +1,313 @@
+<template>
+  <div id="matchHistoryView">
+    <div class="filtersBox">
+      <dropdown 
+        class="dropdown"
+        v-if="matches"
+        :options=timeframeList
+        :label=timeframeLabel
+        :custom=timeframeCustom
+        @update-filter="updateFilters" 
+        @custom-selected="showDatePickerBox = true"
+        @custom-deselected="showDatePickerBox = false">
+      </dropdown>
+      <multi-select-dropdown
+        class="dropdown"
+        v-if="matches"
+        :options=modeList
+        :label=modeLabel
+        @update-filters="updateFilters">
+      </multi-select-dropdown>
+      <multi-select-dropdown
+        class="dropdown"
+        v-if="matches"
+        :options=deckList
+        :label=deckLabel
+        @update-filters="updateFilters">
+      </multi-select-dropdown>
+      <div 
+        class="date-picker-box"
+        v-show="showDatePickerBox">
+        <datepicker placeholder="Start date" v-model="dateStart" :disabledDates="disabledStartDates" v-on:input="updateTimeframeFilter"></datepicker>
+        <span>to</span>
+        <datepicker placeholder="End date" v-model="dateEnd" :disabledDates="disabledEndDates" v-on:input="updateTimeframeFilter"></datepicker>
+      </div>
+    </div>
+    <div class="statsBox">
+      <!-- <h2>Statistics</h2> -->
+      <div v-if="stats">
+        <p class="stat-line">Match win percentage: {{ stats.matchWinPct | toTwoDigits }}%</p>
+        <p class="stat-line">Average match duration: {{ stats.matchAvgTime | secondsToMinutes }}</p>
+        <p class="stat-line">Game win percentage: {{ stats.gameWinPct | toTwoDigits }}%</p>
+        <p class="stat-line">Average game duration: {{ stats.gameAvgTime | secondsToMinutes }}</p>
+      </div>
+    </div>
+    <div class="matchListBox">
+      <!-- <h2>Matches</h2> -->
+      <ul class="matchList" v-if="sortedFilteredMatches.length > 0">
+        <match v-for="match in sortedFilteredMatches" :key=match.matchId :match=match></match>
+      </ul>
+      <p class="noMatches" v-if="(sortedFilteredMatches) && (sortedFilteredMatches.length === 0)">Sorry, no matches were found. :(<br><br>Try again using different search criteria.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Match from './Match.vue'
+  import Dropdown from './Dropdown.vue'
+  import ZerorpcClient from '../zerorpcClient.js'
+  import Datepicker from 'vuejs-datepicker'
+  import MultiSelectDropdown from './MultiSelectDropdown.vue'
+
+  export default {
+    name: 'match-history-view',
+    components: {
+      Match,
+      Dropdown,
+      Datepicker,
+      MultiSelectDropdown
+    },
+    data () {
+      return {
+        matches: null,
+        filteredMatches: null,
+        deckLabel: 'decks',
+        modeLabel: 'modes',
+        timeframeLabel: 'timeframe',
+        timeframeList: ['All Time', 'Today', '7 Days', '30 Days', '365 Days'],
+        timeframeCustom: 'Custom Range',
+        filters: {
+          decks: null,
+          modes: null,
+          timeframe: null
+        },
+        showDatePickerBox: false,
+        dateStart: null,
+        dateEnd: null,
+        stats: null
+      }
+    },
+    computed: {
+      deckList () {
+        let deckList = {}
+        this.matches.forEach((match) => {
+          if (!deckList.hasOwnProperty(match.deckId)) {
+            deckList[match.deckId] = {id: match.deckId, name: match.deckName}
+          }
+        })
+        return deckList
+      },
+      modeList () {
+        let modeList = []
+        this.matches.forEach((match) => {
+          if (!modeList.includes(match.gameType)) {
+            modeList.push(match.gameType)
+          }
+        })
+        return modeList
+      },
+      sortedFilteredMatches () {
+        if (!this.filteredMatches) {
+          return this.matches
+        } else {
+          return this.filteredMatches.slice(0).sort((a,b) => {
+            return b.timestampEnd - a.timestampEnd
+        })
+        }
+      },
+      disabledStartDates () {
+        return {
+          from: this.dateEnd ? this.dateEnd : new Date()
+        }
+      },
+      disabledEndDates () {
+        return {
+          to: this.dateStart ? this.dateStart : new Date(1970, 0, 1),
+          from: new Date()
+        }
+      }
+    },
+    methods: {
+      updateFilters(filter) {
+          switch (filter.type) {
+            case 'modes':
+              if (filter.filters.length === 0) {
+                this.filters[filter.type] = this.modeList
+              } else {
+                this.filters[filter.type] = filter.filters 
+              }
+              break;
+            case 'decks':
+              if (filter.filters.length === 0) {
+                this.filters[filter.type] = Object.keys(this.deckList)
+              } else {
+                let deckIds = []
+                filter.filters.forEach((deck) => {
+                  deckIds.push(deck.id)
+                })
+                this.filters[filter.type] = deckIds 
+              }
+              break;
+            case 'timeframe':
+              this.filters[filter.type] = filter.filter
+          }
+        },
+        sendFilters() {
+          const zClient = new ZerorpcClient();
+          zClient.getMatches(this.filters, (err, res) => {
+            if (!err) {
+              this.filteredMatches = res.matches
+              this.stats = res.stats
+            }
+          })
+        },
+        updateTimeframeFilter () {
+          if (this.dateStart && this.dateEnd) {
+            this.filters.timeframe = {
+              start: this.dateEnd.toLocaleDateString(),
+              end: this.dateEnd.toLocaleDateString()
+            }
+          }
+        }
+    },
+    watch: {
+        'filters.timeframe': function() {
+          this.sendFilters()
+        },
+        'filters.modes': function() {
+          this.sendFilters()
+        },
+        'filters.decks': function() {
+          this.sendFilters()
+        }
+    },
+    filters: {
+      toTwoDigits(val) {
+        if (typeof val == 'string') {
+          val = parseFloat(val)
+        }
+        return val.toFixed(2)
+      },
+      secondsToMinutes(val) {
+        // Round to nearest second
+        val = Math.round(val)
+        return String(Math.floor(val / 60)) + ':' + String(val % 60)
+      }
+    },
+    created () {
+      const zClient = new ZerorpcClient()
+      zClient.getAllMatches((err, res) => {
+          if (!err) {
+          this.matches = res
+          this.filteredMatches = res
+          this.filters.decks = Object.keys(this.deckList)
+          this.filters.modes = this.modeList
+          this.filters.timeframe = 'All Time'
+        }
+      })
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Montserrat');
+  #matchHistoryView {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+  .dropdown {
+    width: 30%;
+  }
+  /* Datepicker Styles */
+  .date-picker-box {
+    width: 100%;
+    padding-left: 1em;
+    padding-top:.5em; 
+    font-size: .9em;
+  }
+  .vdp-datepicker {
+    padding: .5em;
+    display: inline-block;
+  }
+  .vdp-datepicker input {
+    border: none;
+    background-color: #19181A;
+    padding: .5em;
+    cursor: pointer;
+    width: 100px;
+    font-family: inherit;
+    color: rgba(255,255,255,.8);
+    text-align: center;
+    border-radius: 5px;
+  }
+  .vdp-datepicker input:hover {
+    color: #aaa;
+  }
+  .vdp-datepicker__calendar {
+    background-color: #080808;   
+    color: rgba(255,255,255,.9);
+  }
+  .vdp-datepicker__calendar header .next:after {
+    border-left-color: rgba(255,255,255,.9);
+  }
+  .vdp-datepicker__calendar header .next.disabled:after {
+    border: none;
+  }
+  .vdp-datepicker__calendar header .prev:after {
+    border-right-color: rgba(255,255,255,.9);
+  }
+  .vdp-datepicker__calendar header .prev.disabled:after {
+    border: none;
+  }
+  .filtersBox {
+    width: 80%;
+    margin: 40px;
+    margin-bottom: 10px;
+    text-align: left;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    color: rgba(255,255,255,.9);
+  }
+  .matchListBox {
+    width: 60vw;
+    height: 80vh;
+  }
+  .matchList {
+    list-style-type: none;
+    margin: 0 0;
+    border: 1px solid rgba(144,238,144,1);
+    margin: 20px 15px 20px 15px;
+    padding: 0;
+    max-height: 55vh;
+    overflow-y: scroll;
+  }
+  .statsBox {
+    width: 100%;
+    padding-left: 3%;
+    margin: 10px;
+  }
+  .stat-line {
+    width: 20%;
+    display: inline-block;
+    margin-top: 5px;
+    font-size: .8em;
+    text-align: left;
+  }
+  h2 {
+    font-weight: lighter;
+    margin: 0;
+  }
+  .match:last-child {
+    border-bottom: 0px;
+  }
+  .recentStats {
+    padding: 10px;
+  }
+  .date-picker {
+    color: black;
+  }
+</style>
